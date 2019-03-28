5a2a35112f6ddde24143a5eefef9ebe7aec02249
maxidea <maxidea1024@gmail.com>
2019-03-28 22:43:25 +0900

    Added fun::ServerApplication

--- hello.cc
+++ hello.cc
@@ -1,6 +1,41 @@
-#include "fun/fun.h"
+#include <stdio.h>
 
-// branch1
+class Application : public fun::ServerApplication {
+ public:
+  Application() {
+    // Do something...
+  }
+
+  bool Initialize() override {
+    if (!fun::ServerApplication::Initialize()) {
+      return false;
+    }
+
+    json::Value json;
+    if (json.LoadFromFile(filename)) {
+      server.LoadConfigFromJson(json);
+    } else {
+      server.LoadDefaultConfig();
+    }
+
+    return true;
+  }
+
+  void Close() override {
+    // do something...
+
+    fun::ServerApplication::Close();
+  }
+
+ private:
+  fun::Server server;
+}
+
 int main(int argc, char* argv) {
-    return 0;
+  Application app;
+  if (!app.Initialize(argc, argv)) {
+    return -1;
+  }
+  app.Run();
+  return 0;
 }
